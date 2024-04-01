774d7dc41d3d40a13c8479ff297eabe25ddae98c
horsicq <horsicq@gmail.com>
2024-04-01 20:10:50 +0200

    Update file(s): 2024-04-01

--- xdebugscript.cmake
+++ xdebugscript.cmake
@@ -1,1 +1,10 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
+
+set(XDEBUGSCRIPT_SOURCES
+    ${CMAKE_CURRENT_LIST_DIR}/xdebugscript.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xdebugscript.h
+    ${CMAKE_CURRENT_LIST_DIR}/xdebugscriptengine.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xdebugscriptengine.h
+    ${CMAKE_CURRENT_LIST_DIR}/xdebugscripthelper.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xdebugscripthelper.h
+)
